34251288e4d963fc390ed3e9b3247d6db4add9c7
Ruturaj Kundekar <ruturajkundekar77@gmail.com>
2025-12-11 09:06:43 +0530

    Update pract001.txt

--- pract001.php
+++ pract001.php
@@ -1,36 +1,20 @@
-# Question: Write a Python code for Gauss Elimination method
+7. Write a Python program to express a vector as a linear combination of
+vectors
 
-import numpy as np
+import sympy as sp
+A = [1, 1, 1, 1]
+B = [1, 2, 3, 2]
+C = [2, 5, 6, 4]
+M = [3, 3, 5, 4]
+v1 = sp.Matrix(A).reshape(4,1)
+v2 = sp.Matrix(B).reshape(4,1)
+v3 = sp.Matrix(C).reshape(4,1)
+v = sp.Matrix(M).reshape(4,1)
+Mat = sp.Matrix.hstack(v1, v2, v3)
+solution = Mat.gauss_jordan_solve(v)[0]
+c1, c2, c3 = solution
+print("Coefficients:")
+print("c1 =", c1, "c2 =", c2, "c3 =", c3)
 
-a = np.array([[2, -1, 3],
-              [0,  1.5, -0.5],
-              [0,  0,   0.66666667]], float)
-
-b = np.array([9, 1.5, 2], float)
-n = len(b)
-
-# Forward elimination (matrix is already upper-triangular here,
-# but code is written in general form)
-for i in range(n - 1):
-    for j in range(i + 1, n):
-        if a[j, i] == 0:
-            continue
-        factor = a[j, i] / a[i, i]
-        for k in range(i, n):
-            a[j, k] = a[j, k] - a[i, k] * factor
-        b[j] = b[j] - b[i] * factor
-
-print('a=', a)
-print('b=', b)
-
-# Back substitution
-x = np.zeros(n)
-x[n - 1] = b[n - 1] / a[n - 1, n - 1]
-
-for i in range(n - 2, -1, -1):
-    s = 0
-    for j in range(i + 1, n):
-        s += a[i, j] * x[j]
-    x[i] = (b[i] - s) / a[i, i]
-
-print("x=", x)
+Coefficients:
+c1 = 2 c2 = 3 c3 = -1
