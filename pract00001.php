862886fcccb5a62f44991d5118f6a5eba890376b
Ruturaj Kundekar <ruturajkundekar77@gmail.com>
2026-01-07 09:49:48 +0530

    Update pract00001.php

--- pract00001.php
+++ pract00001.php
@@ -1,19 +1,39 @@
-4. Write a program to Show how to handle an exception safely inside a 
-destructor. 
-#include <iostream> 
-using namespace std; 
-class Test { 
-public: 
-~Test() { 
-try { 
-throw "Error in destructor"; 
-} 
-catch (const char* msg) { 
-cout << msg; 
-} 
-} 
-}; 
-int main() { 
-Test t; 
-return 0; 
+4. A small coaching institute wants to store the marks of 5 students in a subject. Write a 
+C++ program to store the marks, accept input from the user, and display the stored marks.
+Functional Requirements:
+• Create a class named StudentMarks.
+• Declare an integer array inside the class to store marks of 5 students.
+• Include a member function named inputMarks() to accept the marks from the user.
+• Include a member function named displayMarks() to display the stored marks.
+• In the main() function, create an object of the class and use it to call inputMarks() and 
+displayMarks().
+#include <iostream>
+using namespace std;
+class StudentMarks
+{
+ int marks[5];
+public:
+ void inputMarks()
+ {
+ cout << "Enter marks of 5 students:\n";
+ for (int i = 0; i < 5; i++)
+ {
+ cin >> marks[i];
+ }
+ }
+ void displayMarks()
+ {
+ cout << "\nMarks of students are:\n";
+for (int i = 0; i < 5; i++)
+ {
+ cout << marks[i] << endl;
+ }
+ }
+};
+int main()
+{
+ StudentMarks sm;
+ sm.inputMarks();
+ sm.displayMarks();
+ return 0;
 }
