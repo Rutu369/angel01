59b5082dcae5bc142537de56e4b90a5611a3834f
Ruturaj Kundekar <ruturajkundekar77@gmail.com>
2026-01-07 09:47:21 +0530

    Update pract001.php

--- pract001.php
+++ pract001.php
@@ -1,21 +1,13 @@
-2. Write a program to Create a class where the constructor throws an 
-exception if marks exceed 100. 
-#include <iostream> 
-using namespace std; 
-class Student { 
-public: 
-Student(int marks) { 
-if (marks > 100) 
-throw "Invalid marks"; 
-cout << "Student created"; 
-} 
-}; 
-int main() { 
-try { 
-Student s(120); 
-} 
-catch (const char* msg) { 
-cout << msg; 
-} 
-return 0; 
-} 
+2. A school wants to store the marks of 5 students that are already known. Write a C++ 
+program to initialize an array with these marks and display them on the screen. Use 
+appropriate loops and clearly show the marks of all students.
+#include<iostream>
+using namespace std;
+int main() {
+ int marks[5] = {78, 85, 90, 67, 88};
+ cout << "Marks of students are:\n";
+ for(int i = 0; i < 5; i++) {
+ cout << "Student " << i + 1 << ": " << marks[i] << endl;
+ }
+ return 0;
+}
