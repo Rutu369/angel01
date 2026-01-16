1a2b286dcb777e8213b6952d105959f2b8dbb98c
Ruturaj Kundekar <ruturajkundekar77@gmail.com>
2026-01-16 08:00:11 +0530

    Update pract001.php

--- pract001.php
+++ pract001.php
@@ -1,63 +1,83 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Marks Evaluation</title>
-</head>
-<body>
-
-<h2>Enter Student Details</h2>
-
-<form method="post" action="process.php">
-    <label>Name:</label>
-    <input type="text" name="name"><br><br>
-
-    <label>Roll No:</label>
-    <input type="text" name="rollno"><br><br>
-
-    <label>Total Marks:</label>
-    <input type="number" name="marks"><br><br>
-
-    <input type="submit" value="submit">
-</form>
-
-</body>
-</html>
-
-
-
-<?php
-function showDetails($student) {
-    echo "<h2>Student Details</h2>";
-    echo "Name: " . $student["name"] . "<br>";
-    echo "Roll No: " . $student["rollno"] . "<br>";
-    echo "Marks: " . $student["marks"] . "<br>";
-
-    $marks = $student["marks"];
-
-    if ($marks > 700) {
-        echo "Marks should be under 700!";
-        return; // stop further execution
-    } elseif ($marks >= 600) {
-        $class = "First Class";
-    } elseif ($marks >= 500) {
-        $class = "Second Class";
-    } elseif ($marks >= 300) {
-        $class = "Third Class";
-    } else {
-        $class = "Not Passed semester exam";
-    }
-
-    echo "Result: " . $class;
+#include <iostream> 
+#include <string> 
+using namespace std; 
+class Laptop  
+{ 
+public: 
+int id; 
+string brand; 
+string processor; 
+int ram; 
+float price; 
+// Function to input data 
+void getData() 
+{ 
+cout << "Enter Laptop ID: "; 
+cin >> id; 
+cout << "Enter Brand Name: "; 
+cin >> brand; 
+cout << "Enter Processor Type: "; 
+cin >> processor; 
+cout << "Enter RAM (GB): "; 
+cin >> ram; 
+        cout << "Enter Price: "; 
+        cin >> price; 
+    } 
+ 
+    // Function to display data 
+    void display()  
+{ 
+        cout << id << "\t" << brand << "\t" 
+             << processor << "\t\t" 
+             << ram << "GB\t" 
+             << price << endl;      // /t create spacing or column 
+    } 
+}; 
+ 
+int main()  
+{ 
+    int n; 
+    cout << "Enter number of laptops: "; 
+    cin >> n; 
+ 
+    Laptop l[n];     //array of Laptop objects ,Each element stores one laptop’s details 
+ 
+    // Input laptop details 
+    for (int i = 0; i < n; i++)  
+  { 
+        cout << "\nEnter details of Laptop " << i + 1 << endl; 
+        l[i].getData(); 
+     } 
+ 
+    // Display all laptops 
+    cout << "\nAll Laptop Records:\n"; 
+    cout << "ID\tBrand\tProcessor\tRAM\tPrice\n";  //Prints heading for table 
+    for (int i = 0; i < n; i++)   
+   { 
+        l[i].display();    // calls display function for every laptop 
+    } 
+ 
+    // Display laptops with price less than 50000 
+    cout << "\nLaptops with price less than 50000:\n"; 
+    cout << "ID\tBrand\tProcessor\tRAM\tPrice\n"; 
+    for (int i = 0; i < n; i++)  
+   { 
+        if (l[i].price < 50000) { 
+l[i].display(); 
+} 
+} 
+// Search by brand 
+string searchBrand; 
+cout << "\nEnter brand to search: "; 
+cin >> searchBrand; 
+cout << "\nLaptops of brand " << searchBrand << ":\n"; 
+cout << "ID\tBrand\tProcessor\tRAM\tPrice\n"; 
+for (int i = 0; i < n; i++)  
+{
+if (l[i].brand == searchBrand)  
+{
+l[i].display(); 
 }
-
-if ($_POST["name"] != "" && $_POST["rollno"] != "" && $_POST["marks"] != "") {
-    $student = [
-        "name" => $_POST["name"],
-        "rollno" => $_POST["rollno"],
-        "marks" => $_POST["marks"]
-    ];
-    showDetails($student);
-} else {
-    echo "Please enter Name, Roll No, and Marks!";
 }
-?>
+return 0; 
+}
